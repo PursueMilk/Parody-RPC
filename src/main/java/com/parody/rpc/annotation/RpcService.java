7134e9010a4c0bea7850042d6e01af1ae9147dae
unicorn <1437121910@qq.com>
2023-01-13 19:22:56 +0800

    注解

---
+++ src/main/java/com/parody/rpc/annotation/RpcService.java
@@ -0,0 +1,15 @@
+package com.parody.rpc.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RpcService {
+    Class<?> interfaceType();
+
+    String version() default "1.0";
+}
