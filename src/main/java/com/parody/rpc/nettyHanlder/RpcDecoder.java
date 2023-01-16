552f21f18c1b90365b76330054e46078dfe32257
unicorn <1437121910@qq.com>
2023-01-16 20:47:02 +0800

    编解码

---
+++ src/main/java/com/parody/rpc/nettyHanlder/RpcDecoder.java
@@ -0,0 +1,48 @@
+package com.parody.rpc.nettyHanlder;
+
+import com.parody.rpc.protocol.MessageHeader;
+import com.parody.rpc.protocol.MessageProtocol;
+import com.parody.rpc.serialization.RpcSerialization;
+import com.parody.rpc.serialization.SerializationFactory;
+import com.parody.rpc.serialization.SerializationTypeEnum;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class RpcDecoder<T> extends MessageToByteEncoder<MessageProtocol<T>> {
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, MessageProtocol<T> messageProtocol, ByteBuf byteBuf) throws Exception {
+//        log.info("{}",messageProtocol);
+        MessageHeader header = messageProtocol.getHeader();
+        // 魔数
+        byteBuf.writeInt(header.getMagic());
+
+        // 协议版本号
+        byteBuf.writeByte(header.getVersion());
+
+        // 序列化算法
+        byteBuf.writeByte(header.getSerialization());
+
+        // 报文类型
+        byteBuf.writeByte(header.getMsgType());
+
+        // 消息 ID
+        byteBuf.writeCharSequence(header.getRequestId(), Charset.forName("UTF-8"));
+
+
+        RpcSerialization rpcSerialization = SerializationFactory.getRpcSerialization(SerializationTypeEnum.parseByType(header.getSerialization()));
+
+        byte[] data = rpcSerialization.serialize(messageProtocol.getBody());
+
+        // 数据长度
+        byteBuf.writeInt(data.length);
+
+        // 数据内容
+        byteBuf.writeBytes(data);
+    }
+}
---
+++ src/main/java/com/parody/rpc/nettyHanlder/RpcEncoder.java
@@ -0,0 +1,99 @@
+package com.parody.rpc.nettyHanlder;
+
+import com.parody.rpc.message.HeartBeatMessage;
+import com.parody.rpc.message.RpcRequest;
+import com.parody.rpc.message.RpcResponse;
+import com.parody.rpc.protocol.MessageHeader;
+import com.parody.rpc.protocol.MessageProtocol;
+import com.parody.rpc.protocol.MsgType;
+import com.parody.rpc.protocol.ProtocolConstants;
+import com.parody.rpc.serialization.RpcSerialization;
+import com.parody.rpc.serialization.SerializationFactory;
+import com.parody.rpc.serialization.SerializationTypeEnum;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.MessageToByteEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+@Slf4j
+public class RpcEncoder extends ByteToMessageDecoder{
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() < ProtocolConstants.HEADER_TOTAL_LEN) {
+            // 可读的数据小于请求头总的大小 直接丢弃
+            return;
+        }
+        // 标记 ByteBuf 读指针位置
+        in.markReaderIndex();
+
+        // 魔数
+        int magic = in.readInt();
+        if (magic != ProtocolConstants.MAGIC) {
+            throw new IllegalArgumentException("magic number is illegal, " + magic);
+        }
+
+        byte version = in.readByte();
+        byte serializeType = in.readByte();
+        byte msgType = in.readByte();
+        CharSequence requestId = in.readCharSequence(ProtocolConstants.REQ_LEN, Charset.forName("UTF-8"));
+
+        int dataLength = in.readInt();
+        if (in.readableBytes() < dataLength) {
+            // 可读的数据长度小于 请求体长度 直接丢弃并重置 读指针位置
+            in.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+
+        MsgType msgTypeEnum = MsgType.findByType(msgType);
+        if (msgTypeEnum == null) {
+            return;
+        }
+
+        MessageHeader header = new MessageHeader();
+        header.setMagic(magic);
+        header.setVersion(version);
+        header.setSerialization(serializeType);
+        header.setRequestId(String.valueOf(requestId));
+        header.setMsgType(msgType);
+        header.setMsgLen(dataLength);
+        log.info("{}",header);
+
+        RpcSerialization rpcSerialization = SerializationFactory.getRpcSerialization(SerializationTypeEnum.parseByType(serializeType));
+        switch (msgTypeEnum) {
+            case REQUEST:
+                RpcRequest request = rpcSerialization.deserialize(data, RpcRequest.class);
+                if (request != null) {
+                    MessageProtocol<RpcRequest> protocol = new MessageProtocol<>();
+                    protocol.setHeader(header);
+                    protocol.setBody(request);
+                    out.add(protocol);
+                }
+                break;
+            case RESPONSE:
+                RpcResponse response = rpcSerialization.deserialize(data, RpcResponse.class);
+                if (response != null) {
+                    MessageProtocol<RpcResponse> protocol = new MessageProtocol<>();
+                    protocol.setHeader(header);
+                    protocol.setBody(response);
+                    out.add(protocol);
+                }
+                break;
+            case HEART:
+                HeartBeatMessage heartBeatMessage = rpcSerialization.deserialize(data, HeartBeatMessage.class);
+                if (heartBeatMessage != null) {
+                    MessageProtocol<HeartBeatMessage> protocol = new MessageProtocol<>();
+                    protocol.setHeader(header);
+                    protocol.setBody(heartBeatMessage);
+                    out.add(protocol);
+                }
+                break;
+        }
+    }
+}
