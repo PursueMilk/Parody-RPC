7337376e89b90f7b1e6468cc175125c90b055aac
unicorn <1437121910@qq.com>
2023-01-18 20:02:13 +0800

    配置类

---
+++ src/main/java/com/parody/rpc/config/RpcClientConfig.java
@@ -0,0 +1,44 @@
+package com.parody.rpc.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Objects;
+import java.util.Properties;
+
+public class RpcClientConfig {
+
+    private static Properties properties;
+
+    //读取application.properties文件
+    static {
+        try (InputStream in = RpcClientConfig.class.getResourceAsStream("/application.properties")) {
+            properties = new Properties();
+            properties.load(in);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    /**
+     * 给RpcClientProperties对象的属性设置值
+     *
+     * @param clientProperties
+     * @throws IllegalAccessException
+     */
+    public static void fillProperties(RpcClientProperties clientProperties) throws IllegalAccessException {
+        Field[] fields = clientProperties.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            String name = "client." + field.getName();
+            String value = properties.getProperty(name);
+            if (Objects.nonNull(value)) {
+                field.setAccessible(true);
+                if (name.equals("client.timeout")) {
+                    field.set(clientProperties, Integer.valueOf(value));
+                } else {
+                    field.set(clientProperties, value);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/parody/rpc/config/RpcClientProperties.java
@@ -0,0 +1,49 @@
+package com.parody.rpc.config;
+
+import lombok.Data;
+
+
+@Data
+public class RpcClientProperties {
+
+    /**
+     * 负载均衡
+     */
+    private String balance="RANDOM";
+
+    /**
+     * 序列化
+     */
+    private String serialization = "JSON";
+
+    /**
+     * 服务发现地址
+     */
+    private String discoveryAddress = "127.0.0.1:8848";
+
+    /**
+     * 服务调用超时（毫秒）
+     */
+    private Integer timeout=3000;
+
+
+    /**
+     * 服务版本号
+     */
+    private String version = "1.0";
+
+    /**
+     * 获取配置类对象
+     * @return
+     */
+    public static RpcClientProperties getProperties() {
+        RpcClientProperties rpcClientProperties = new RpcClientProperties();
+        try {
+            //加载自定义配置
+            RpcClientConfig.fillProperties(rpcClientProperties);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return rpcClientProperties;
+    }
+}
