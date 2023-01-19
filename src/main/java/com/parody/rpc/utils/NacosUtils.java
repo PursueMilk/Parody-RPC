018664da752b696a3ef362f4f2041c0c0415ac89
unicorn <1437121910@qq.com>
2023-01-19 23:12:56 +0800

    工具类

---
+++ src/main/java/com/parody/rpc/utils/NacosUtils.java
@@ -0,0 +1,87 @@
+package com.parody.rpc.utils;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.parody.rpc.balancer.LoadBalance;
+import com.parody.rpc.balancer.LoadBalanceFactory;
+import com.parody.rpc.config.RpcClientProperties;
+
+import java.net.InetSocketAddress;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class NacosUtils {
+
+    private static final NamingService namingService;
+
+    private static final Set<String> serviceNames = new HashSet<>();
+
+    private static InetSocketAddress address;
+
+    private static final RpcClientProperties properties;
+
+    static {
+        properties = RpcClientProperties.getProperties();
+        namingService = getNacosNamingService();
+    }
+
+    //初始化
+    public static NamingService getNacosNamingService() {
+        try {
+            return NamingFactory.createNamingService(properties.getDiscoveryAddress());
+        } catch (NacosException e) {
+            throw new RuntimeException("连接到Nacos时发生错误");
+        }
+    }
+
+    /**
+     * 注册服务
+     *
+     * @param serverName
+     * @param address
+     * @throws NacosException
+     */
+    public static void registerServer(String serverName, InetSocketAddress address) throws NacosException {
+        namingService.registerInstance(serverName, address.getHostName(), address.getPort());
+        NacosUtils.address = address;
+        serviceNames.add(serverName);
+    }
+
+
+    /**
+     * 根据负载均衡算法获取当前服务名中的实例
+     * @param serverName
+     * @return
+     * @throws NacosException
+     */
+    public static Instance getInstance(String serverName) throws NacosException {
+        List<Instance> list = namingService.getAllInstances(serverName);
+        LoadBalance balance = LoadBalanceFactory.getLoadBalance(properties.getBalance());
+        return balance.getInstance(list);
+    }
+
+    /**
+     * 注销服务
+     */
+    public static void clearRegister() {
+        if (!serviceNames.isEmpty() && address != null) {
+            String host = address.getHostName();
+            int port = address.getPort();
+            Iterator<String> iterator = serviceNames.iterator();
+            while (iterator.hasNext()) {
+                String serviceName = iterator.next();
+                try {
+                    namingService.deregisterInstance(serviceName, host, port);
+                } catch (NacosException e) {
+                    new RuntimeException("注销服务失败");
+                }
+            }
+        }
+    }
+
+
+}
